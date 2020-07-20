1e37b01f572f4d9e52968d894ac525d77cd8820b
szymeq2k2 <67710719+szymeq2k2@users.noreply.github.com>
2020-07-20 14:00:55 +0200

    Update app.py
    deleting unnecesary code

--- app.py
+++ app.py
@@ -11,100 +11,114 @@ import numpy as np
 import re
 import dash
 
+def forecastData():
+    request_for = requests.get("http://api.openweathermap.org/data/2.5/forecast?lat=50.040003&lon=18.394399&APPID=7cf51f9cd2de7b8e469a6e0ea1bf986c&units=metric")
+    for_weather = request_for.content.strip()
+    data_for = json.loads(for_weather)
+    return data_for
 
-request_for = requests.get("http://api.openweathermap.org/data/2.5/forecast?lat=50.040003&lon=18.394399&APPID=7cf51f9cd2de7b8e469a6e0ea1bf986c&units=metric")
-for_weather = request_for.content.strip()
-json_data_for = json.loads(for_weather)
-
-
-request_loc = requests.get("http://api.openweathermap.org/data/2.5/weather?lat=50.040003&lon=18.394399&APPID=7cf51f9cd2de7b8e469a6e0ea1bf986c&units=metric")
-loc_weather = request_loc.content.strip()
-json_data = json.loads(loc_weather)
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+def currentData():
+    request_loc = requests.get("http://api.openweathermap.org/data/2.5/weather?lat=50.040003&lon=18.394399&APPID=7cf51f9cd2de7b8e469a6e0ea1bf986c&units=metric")
+    loc_weather = request_loc.content.strip()
+    data = json.loads(loc_weather)
+    return data
 
 
-data = {'temp': json_data['main']['temp'], 'feels like': json_data['main']['feels_like'], 'temp min': json_data['main']['temp_min'], 'temp max':json_data['main']['temp_max'] }
-names = list(data.keys())
-values = list(data.values())
-df1 = pd.DataFrame({"x": names,"y": values})
-fig1 = px.bar(df1, x="x",y="y" ,barmode="group")
+def creating_dash_board():
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-values=[json_data['wind']['speed'],json_data['wind']['speed']]
-names = ["speed","gust"]
-df2 =pd.DataFrame({"x":names,"y":values})
-fig2 = px.bar(df2,x="x",y="y",barmode="group")
 
-values=[json_data['main']['humidity']]
-names=["humidity"]
-df3 =pd.DataFrame({"x":names,"y":values})
-fig3 = px.bar(df3,x="x",y="y",barmode="group")
+    data = {'temp': json_data['main']['temp'], 'feels like': json_data['main']['feels_like'], 'temp min': json_data['main']['temp_min'], 'temp max':json_data['main']['temp_max'] }
+    names = list(data.keys())
+    values = list(data.values())
+    df1 = pd.DataFrame({"x": names,"y": values})
+    fig1 = px.bar(df1, x="x",y="y" ,barmode="group")
 
-values=[json_data_for['list'][0]['main']['temp'],json_data_for['list'][1]['main']['temp'],json_data_for['list'][2]['main']['temp'],json_data_for['list'][3]['main']['temp'],json_data_for['list'][4]['main']['temp']]
-names=[1,2,3,4,5]
-df4 =pd.DataFrame({"x":names,"y":values})
-fig4 = px.line(df4,x="x",y="y")
+    values=[json_data['wind']['speed'],json_data['wind']['speed']]
+    names = ["speed","gust"]
+    df2 =pd.DataFrame({"x":names,"y":values})
+    fig2 = px.bar(df2,x="x",y="y",barmode="group")
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
+    values=[json_data['main']['humidity']]
+    names=["humidity"]
+    df3 =pd.DataFrame({"x":names,"y":values})
+    fig3 = px.bar(df3,x="x",y="y",barmode="group")
 
-app.layout = html.Div(children=[
+    values=[json_data_for['list'][0]['main']['temp'],json_data_for['list'][1]['main']['temp'],json_data_for['list'][2]['main']['temp'],json_data_for['list'][3]['main']['temp'],json_data_for['list'][4]['main']['temp']]
+    names=[1,2,3,4,5]
+    df4 =pd.DataFrame({"x":names,"y":values})
+    fig4 = px.line(df4,x="x",y="y")
 
-    html.H1(
-    children='Temperature today ',
-    style={
-        'textAlign': 'center',
-        'color': colors['text']
+    colors = {
+        'background': '#111111',
+        'text': '#7FDBFF'
     }
-    ),
 
-    dcc.Graph(
-        id='temp',
-        figure=fig1
-    ),
+    app.layout = html.Div(children=[
 
-    html.H1(
-    children='Wind today ',
-    style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }
-    ),
+        html.H1(
+        children='Temperature today ',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+        ),
 
-    dcc.Graph(
-        id='wind',
-        figure=fig2
-    ),
+        dcc.Graph(
+            id='temp',
+            figure=fig1
+        ),
 
-    html.H1(
-    children='Humid today ',
-    style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }
-    ),
+        html.H1(
+        children='Wind today ',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+        ),
 
-    dcc.Graph(
-        id='humid',
-        figure=fig3
-    ),
+        dcc.Graph(
+            id='wind',
+            figure=fig2
+        ),
 
-    html.H1(
-    children='Temperature in future ',
-    style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }
-    ),
+        html.H1(
+        children='Humid today ',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+        ),
 
-    dcc.Graph(
-        id='future',
-        figure=fig4
-    )
+        dcc.Graph(
+            id='humid',
+            figure=fig3
+        ),
 
-])
+        html.H1(
+        children='Temperature in future ',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+        ),
+
+        dcc.Graph(
+            id='future',
+            figure=fig4
+        )
+
+    ])
+    return app
+
+json_data = currentData()
+json_data_for = forecastData()
+
+
+
+
+
+
+
 
