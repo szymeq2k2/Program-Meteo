5e5f5a6caf8441b9cd5a92e1b8816809451374ad
szymeq2k2 <67710719+szymeq2k2@users.noreply.github.com>
2020-07-22 13:55:24 +0200

    added dark mode
    i cant delete margin

--- pszow_meteo.py
+++ pszow_meteo.py
@@ -14,6 +14,8 @@ from meteo_db import max_min_avg
 from meteo_db import add_Data
 from datetime import timedelta
 
+
+
 # forecastData returns forecast data
 def forecastData():
     request_for = requests.get("http://api.openweathermap.org/data/2.5/forecast?lat=50.040003&lon=18.394399&APPID=7cf51f9cd2de7b8e469a6e0ea1bf986c&units=metric")
@@ -32,35 +34,45 @@ def currentData():
 def get_dates(days_pass):
     date_today = datetime.datetime.today()
     date1 = date_today + timedelta(days = days_pass)
-    # return date1.strftime('%Y-%m-%d-%H:%M')
     return date1.strftime('%A')
 
 #creating_dash_board creates a dashboard
-def creating_dash_board(json_data,json_data_for):
+def creating_dash_board(json_data,json_data_for,dark):
+
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
     app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+    app.css.append_css({'external_url': 'reset.css'})
     data = {'temp': json_data['main']['temp'], 'feels like': json_data['main']['feels_like'], 'temp min': json_data['main']['temp_min'], 'temp max':json_data['main']['temp_max'] }
     names = list(data.keys())
     values = list(data.values())
     df1 = pd.DataFrame({"Type": names,"°C": values})
-    fig1 = px.bar(df1, x="Type",y="°C" ,barmode="group")
+    fig1_day = px.bar(df1, x="Type",y="°C" ,barmode="group")
+    fig1_night = px.bar(df1, x="Type",y="°C" ,barmode="group",color_discrete_sequence =['darkblue']*3).update_layout({
+                                    'paper_bgcolor': colors['background'],
+                                    'plot_bgcolor': colors['graph-bg-dark']
+                                    })
 
     wind_speed='Wind speed = '+str(json_data['wind']['speed'])+'km/h'
     wind_gust='Wind gust = '+str(json_data['wind']['speed'])+'km/h'
     humidity_value='Humidity level = '+str(json_data['main']['humidity'])+'%'
 
-    values=[json_data_for['list'][0]['main']['temp'],json_data_for['list'][1]['main']['temp'],json_data_for['list'][2]['main']['temp'],json_data_for['list'][3]['main']['temp'],json_data_for['list'][4]['main']['temp']]
+    values=[json_data_for['list'][1]['main']['temp'],json_data_for['list'][2]['main']['temp'],json_data_for['list'][3]['main']['temp'],json_data_for['list'][4]['main']['temp'],json_data_for['list'][5]['main']['temp']]
     names=[get_dates(1),get_dates(2),get_dates(3),get_dates(4),get_dates(5)]
-    # df4 =pd.DataFrame({"Date":names,"°C":values})
     df4 =pd.DataFrame({"Day":names,"°C":values})
-    fig4 = px.line(df4,x="Day",y="°C")
+    fig4_day = px.line(df4,x="Day",y="°C")
+    fig4_night = px.line(df4,x="Day",y="°C",color_discrete_sequence =['darkblue']*3).update_layout({
+                                    'paper_bgcolor': colors['background'],
+                                    'plot_bgcolor': colors['graph-bg-dark']
+                                    })
+                                    
+    if dark==True:
+        app.layout = create_dash_layout_night(fig1_night,fig4_night,wind_gust,wind_speed,humidity_value,app)
+    else:
+        app.layout = create_dash_layout_day(fig1_day,fig4_day,wind_gust,wind_speed,humidity_value,app)
+    return app
 
-    colors = {
-        'background': '#111111',
-        'text': '#7FDBFF'
-    }
-
+#returns app with bright layout
+def create_dash_layout_day(fig1,fig4,wind_gust,wind_speed,humidity_value,app):
     app.layout = html.Div(children=[
 
         html.H1(
@@ -84,10 +96,17 @@ def creating_dash_board(json_data,json_data_for):
         }
         ),
         html.H2(
-        children=wind_speed
+        children=wind_speed ,
+            style={
+            'textAlign': 'center'
+            }
+        
         ),
         html.H2(
-        children=wind_gust
+        children=wind_gust ,
+            style={
+            'textAlign': 'center'
+            }
         ),
 
         html.H1(
@@ -98,7 +117,10 @@ def creating_dash_board(json_data,json_data_for):
         }
         ),
         html.H2(
-        children=humidity_value
+        children=humidity_value ,
+            style={
+            'textAlign': 'center'
+            }
         ),
 
         html.H1(
@@ -113,9 +135,78 @@ def creating_dash_board(json_data,json_data_for):
             id='future',
             figure=fig4
         )
-
+    
     ])
-    return app
+    return app.layout
+
+#returns app with dark layout
+def create_dash_layout_night(fig1,fig4,wind_gust,wind_speed,humidity_value,app):
+    app.layout = html.Div(style={'backgroundColor': colors['background']},children=[
+        html.H1(
+        children='Temperature today ',
+        style={
+            'textAlign': 'center',
+            'color': colors['text-dark']
+        }
+        ),
+
+        dcc.Graph(
+            id='temp',
+            figure=fig1 ,
+        ),
+
+        html.H1(
+        children='Wind today ',
+        style={
+            'textAlign': 'center',
+            'color': colors['text-dark']
+        }
+        ),
+        html.H2(
+        children=wind_speed ,
+        style={
+            'textAlign': 'center',
+            'color': colors['text-dark']
+        }
+        ),
+        html.H2(
+        children=wind_gust ,
+        style={
+            'textAlign': 'center',
+            'color': colors['text-dark']
+        }
+        ),
+
+        html.H1(
+        children='Humid today ',
+        style={
+            'textAlign': 'center',
+            'color': colors['text-dark']
+        }
+        ),
+        html.H2(
+        children=humidity_value ,
+        style={
+            'textAlign': 'center',
+            'color': colors['text-dark']
+        }
+        ),
+
+        html.H1(
+        children='Temperature in future ',
+        style={
+            'textAlign': 'center',
+            'color': colors['text-dark']
+        }
+        ),
+
+        dcc.Graph(
+            id='future',
+            figure=fig4 ,
+        )
+    
+    ])
+    return app.layout
 
 def start():
     end=False
@@ -132,6 +223,12 @@ def start():
             add_Data(weatherNow)
         elif choice == "show":
             if __name__ == '__main__':
+                app=creating_dash_board(json_data,json_data_for,False)
+                webbrowser.open('http://127.0.0.1:8050/')
+                app.run_server()
+        elif choice == "show -d":
+            if __name__ == '__main__':
+                app=creating_dash_board(json_data,json_data_for,True)
                 webbrowser.open('http://127.0.0.1:8050/')
                 app.run_server()
         elif choice == "today":
@@ -139,12 +236,18 @@ def start():
         elif choice == "quit":
             end=True
 
+colors = {
+    'background': '#1a1a1a',
+    'text': '#7FDBFF',
+    'text-dark': '#00EA64',
+    'graph-bg-dark': '#6E6E6E'
+ }
+
 x = datetime.datetime.today().strftime('%Y-%m-%d-%H:%M')
 # print(x)
 json_data = currentData()
 json_data_for = forecastData()
 avg =(json_data['main']['temp_min']+json_data['main']['temp_max'])/2
-app=creating_dash_board(json_data,json_data_for)
 weatherNow =[(str(x),json_data['main']['temp'],avg,json_data['main']['temp_min'],json_data['main']['temp_max'],json_data['main']['humidity'],json_data['wind']['speed'])]
 
 start()
